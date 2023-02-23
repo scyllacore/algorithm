fa78b13f12e4b6c4ead7f29bd2154f612a5881cd
scyllacore <yms8795@gmail.com>
2023-02-23 21:47:01 +0900

    refactor : 코드 수정 및 재풀이.

--- MYSQL 풀이/programmers/level2/중복 제거하기.sql
+++ MYSQL 풀이/programmers/level2/중복 제거하기.sql
@@ -1,18 +1,5 @@
-SELECT CAT_ANIMAL_INS.ANIMAL_TYPE
-FROM 
-    (
-        SELECT * 
-        FROM ANIMAL_INS
-        WHERE ANIMAL_TYPE = 'CAT'
-    ) CAT_ANIMAL_INS
-    INNER JOIN
-    (
-        SELECT * 
-        FROM ANIMAL_INS
-        WHERE ANIMAL_TYPE = 'DOG'
-    ) DOG_ANIMAL_INS
-WHERE CAT_ANIMAL_INS.NAME = 'Ella' AND DOG_ANIMAL_INS.NAME = 'Bj'
-
+SELECT COUNT(DISTINCT NAME) AS 'count'
+FROM ANIMAL_INS
     
     
 # https://chanhuiseok.github.io/posts/db-4/
