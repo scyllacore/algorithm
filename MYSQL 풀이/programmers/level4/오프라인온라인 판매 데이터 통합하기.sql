afe851fc757cf33222d18677060f8dc1882eae4d
scyllacore <yms8795@gmail.com>
2023-02-26 19:24:17 +0900

    feat : [programmers level 4 (sql]) sql 고득점 kit 1문제 풀이.

---
+++ MYSQL 풀이/programmers/level4/오프라인온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,24 @@
+WITH SALES_DATA AS (
+    SELECT  SALES_DATE
+            , PRODUCT_ID
+            , USER_ID
+            , SALES_AMOUNT
+    FROM    ONLINE_SALE
+        UNION ALL 
+    SELECT  SALES_DATE
+            , PRODUCT_ID
+            , NULL AS USER_ID
+            , SALES_AMOUNT
+    FROM    OFFLINE_SALE
+)
+
+
+SELECT  DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE
+        , PRODUCT_ID
+        , USER_ID
+        , SALES_AMOUNT
+FROM    SALES_DATA
+WHERE   SALES_DATE LIKE '2022-03%'
+ORDER BY    SALES_DATE ASC, PRODUCT_ID ASC, USER_ID ASC
+
+#https://eroul-ri.tistory.com/21
