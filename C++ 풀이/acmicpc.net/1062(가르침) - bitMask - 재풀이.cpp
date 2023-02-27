ed8141b42f5e07819505ab0257b307bad82095a9
scyllacore <yms8795@gmail.com>
2023-02-27 20:45:24 +0900

    refactor : [acmicpc 코테 준비 연습]  가르침 재풀이.

---
+++ C++ 풀이/acmicpc.net/1062(가르침) - bitMask - 재풀이.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int i, j;
+
+	int n, k;
+	cin >> n >> k;
+
+	int* words = new int[n] {0};
+	string str;
+
+	for (i = 0; i < n; i++) {
+		cin >> str;
+
+		for (j = 4; j < str.length() - 4; j++) {
+			words[i] |= 1 << (str[j] - 'a');
+		}
+	}
+
+
+	int alphaCheck = 0;
+	alphaCheck |= 1 << ('a' - 'a');
+	alphaCheck |= 1 << ('c' - 'a');
+	alphaCheck |= 1 << ('i' - 'a');
+	alphaCheck |= 1 << ('n' - 'a');
+	alphaCheck |= 1 << ('t' - 'a');
+
+	int answer = 0;
+
+	for (int bitCase = alphaCheck; bitCase < (1 << 26); bitCase++) {
+
+		if (alphaCheck != (bitCase & alphaCheck)) {
+			continue;
+		}
+
+		int alphaCnt = 0;
+
+		for (i = 0; i < 26; i++) {
+			if (bitCase & (1 << i)) {
+				alphaCnt++;
+			}
+		}
+
+		if (alphaCnt != k) {
+			continue;
+		}
+
+		int readingWord = 0;
+
+		for (i = 0; i < n; i++) {
+			if (words[i] == (bitCase & words[i])) {
+				readingWord++;
+			}
+		}
+
+		answer = max(readingWord, answer);
+	}
+
+	cout << answer;
+	return 0;
+}
