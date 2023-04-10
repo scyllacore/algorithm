9a7edacad2722d5c0197fd025456dcf17062ae13
myungsikyoon <yms8795@gmail.com>
2023-04-10 10:24:55 +0900

    feat : [acmicpc 중급 1/2]  4연산 풀이 완료.

---
+++ C++ 풀이/acmicpc.net/4연산.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <queue>
+#include <unordered_set>
+
+using namespace std;
+
+#define MAX (long long)1e9
+
+struct numInfo {
+	int num;
+	string operStr;
+};
+
+bool isRangeNum(long long val) {
+	return val >= 1 && val <= MAX;
+}
+
+string bfs(int& s, int& t) {
+
+	if (s == t) {
+		return "0";
+	}
+
+	queue<numInfo> bfsQ;
+	unordered_set<int> visited;
+
+	bfsQ.push({ s,"" });
+	visited.insert(s);
+
+	for (; !bfsQ.empty();) {
+		long long curVal = bfsQ.front().num;
+		string curOper = bfsQ.front().operStr;
+		bfsQ.pop();
+
+		if (curVal == t) {
+			return curOper;
+		}
+
+		long long calVal;
+
+		calVal = curVal * curVal;
+		if (isRangeNum(calVal) && visited.find(calVal) == visited.end()) {
+			visited.insert(calVal);
+			bfsQ.push({ (int)calVal,curOper + "*" });
+		}
+
+		calVal = curVal + curVal;
+		if (isRangeNum(calVal) && visited.find(calVal) == visited.end()) {
+			visited.insert(calVal);
+			bfsQ.push({ (int)calVal,curOper + "+" });
+		}
+
+		calVal = curVal - curVal;
+		if (isRangeNum(calVal) && visited.find(calVal) == visited.end()) {
+			visited.insert(calVal);
+			bfsQ.push({ (int)calVal,curOper + "-" });
+		}
+
+		calVal = curVal / curVal;
+		if (isRangeNum(calVal) && visited.find(calVal) == visited.end()) {
+			visited.insert(calVal);
+			bfsQ.push({ (int)calVal,curOper + "/" });
+		}
+	}
+
+
+	return "-1";
+
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int s, t;
+
+	cin >> s >> t;
+
+	cout << bfs(s, t);
+	return 0;
+}
