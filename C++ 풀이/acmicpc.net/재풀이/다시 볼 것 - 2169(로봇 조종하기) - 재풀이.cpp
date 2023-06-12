6fcdd3d93c41a7619cec09a8ebac829d7565901c
scyllacore <yms8795@gmail.com>
2023-06-12 13:30:51 +0900

    refactor : 재풀이.

--- C++ 풀이/acmicpc.net/재풀이/다시 볼 것 - 2169(로봇 조종하기) - 재풀이.cpp
+++ C++ 풀이/acmicpc.net/재풀이/다시 볼 것 - 2169(로봇 조종하기) - 재풀이.cpp
@@ -41,6 +41,7 @@ int dfs(int y, int x, int dir) {
 	visited[y][x] = 0;
 
 	return dp[y][x][dir];
+	//dp[y][x][dir]의 의미는 이전 지점에서 dir방향으로 움직여 현재 y,x 지점으로 접근했다는 것을 나타냄.
 }
 
 int main() {
@@ -58,7 +59,35 @@ int main() {
 
 	fill(&dp[0][0][0], &dp[SIZE - 1][SIZE - 1][3], BOUNDARY);
 
-	int ans = dfs(0, 0, 0);
+	cout << dfs(0, 0, 0); // 시작점에서 방향은 아무거나 상관 없음 
 
-	cout << ans;
 }
+
+/*
+#include <iostream>
+
+using namespace std;
+
+using uint = unsigned;
+using ll = long long;
+using ull = unsigned long long;
+
+int N, M, A[1010][1010], D[1010][1010];
+
+int main() {
+	ios_base::sync_with_stdio(false); cin.tie(nullptr);
+	cin >> N >> M;
+	for (int i = 1; i <= N; i++) for (int j = 1; j <= M; j++) cin >> A[i][j];
+
+	for (int i = 1; i <= M; i++) D[1][i] = D[1][i - 1] + A[1][i];
+	for (int i = 2; i <= N; i++) {
+		int L[1010], R[1010];
+		L[1] = D[i - 1][1] + A[i][1];
+		R[M] = D[i - 1][M] + A[i][M];
+		for (int j = 2; j <= M; j++) L[j] = max(D[i - 1][j], L[j - 1]) + A[i][j];
+		for (int j = M - 1; j; j--) R[j] = max(D[i - 1][j], R[j + 1]) + A[i][j];
+		for (int j = 1; j <= M; j++) D[i][j] = max(L[j], R[j]);
+	}
+	cout << D[N][M];
+}
+*/
